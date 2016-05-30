7c640392dfa914f4678d99733876e144cbfdb553
Colin White <colin@colinwhite.me>
2016-05-30 13:41:52 -0700

    Fix temporary images not being cleaned up correctly on error.

--- src/com/colinrtwhite/Main.java
+++ src/com/colinrtwhite/Main.java
@@ -11,7 +11,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class Main {
+class Main {
 	private static final String CP_COMMAND = "cp -r %s %s";
 	private static final String COMPARE_PNGS_COMMAND = "compare_pngs %s %s";
 	private static final String CWEBP_COMMAND = "cwebp -q %d %s -o %s";
@@ -70,11 +70,11 @@ public class Main {
 
 			// Read in the directory argument then any optional arguments.
 			directory = new File(args[0]);
-			boolean expectsNumCpusNext = false, expectsQualityNext = false;
+			boolean expectsNumCPUsNext = false, expectsQualityNext = false;
 			for (int i = 1; i < args.length; i++) {
 				switch (args[i]) {
 					case "-n":
-						expectsNumCpusNext = true;
+						expectsNumCPUsNext = true;
 						break;
 					case "-q":
 						expectsQualityNext = true;
@@ -86,12 +86,12 @@ public class Main {
 						allowLossless = true;
 						break;
 					default:
-						if (expectsNumCpusNext) {
+						if (expectsNumCPUsNext) {
 							numCPUs = Integer.valueOf(args[i]);
 						} else if (expectsQualityNext) {
 							qualityThreshold = Double.valueOf(args[i]);
 						}
-						expectsNumCpusNext = false;
+						expectsNumCPUsNext = false;
 						expectsQualityNext = false;
 						break;
 				}
@@ -107,50 +107,23 @@ public class Main {
 	}
 
 	private static void traverseDirectory(final File directory) {
-		executor.execute(() -> {
-			try {
-				System.out.println("Compressing files in " + directory.getCanonicalPath() + "...");
-			} catch (IOException e) { /* Do nothing. */ }
-		});
+		executor.execute(() -> System.out.println("Compressing files in " + directory.getPath() + "..."));
 		for (File file : directory.listFiles(imageFilter)) {
 			executor.execute(() -> {
+				// Compute some initial information about the image file.
+				String path = file.getPath();
+				String basePath = path.substring(0, path.lastIndexOf("."));
+				String webPPath = basePath + WEBP;
+				File webPFile = new File(webPPath);
+				boolean needsConvertToPng = !file.getName().endsWith(PNG);
+				boolean hasError = false;
+				totalOriginalSize += file.length();
+
 				try {
-					// Compute some initial information about the image file.
-					String path = file.getCanonicalPath();
-					String basePath = path.substring(0, path.lastIndexOf("."));
-					String webPPath = basePath + WEBP;
-					File webPFile = new File(webPPath);
-					boolean needsConvertToPng = !file.getName().endsWith(PNG);
-					long min = 0, max = 100;
-
-					// Perform binary search to find the best compression value.
-					while (min != max) {
-						// Generate the temporary PNG images.
-						long quality = Math.round((min + max) / 2.0);
-						if (getImageDissimilarity(runtime, quality, path, basePath, webPPath, needsConvertToPng, false) <
-								qualityThreshold && isSmallerFile(webPFile, file)) {
-							// The image can be compressed more.
-							if (max == quality) {
-								// Prevent infinite loops.
-								break;
-							}
-							max = quality;
-						} else {
-							// Compression is now visible to the human eye (or the file got larger).
-							// Scale the compression back a bit (if we can).
-							if (min == quality) {
-								// Prevent infinite loops.
-								break;
-							}
-							min = quality;
-						}
-					}
-
-					if (min == 100) {
-						// Attempt lossless compression if enabled (requires minSdk >= 17 on Android).
-						if (allowLossless &&
-								getImageDissimilarity(runtime, 100, path, basePath, webPPath, needsConvertToPng, true) < qualityThreshold &&
-								isSmallerFile(webPFile, file)) {
+					if (searchForOptimalQuality(file, path, basePath, webPPath, webPFile, needsConvertToPng)) {
+						// Attempt lossless compression if enabled (requires minSdk >= 18 on Android).
+						if (allowLossless && getImageDissimilarity(100, path, basePath, webPPath,
+								needsConvertToPng, true) < qualityThreshold && isSmallerFile(webPFile, file)) {
 							bytesSaved += file.length() - webPFile.length();
 							runtime.exec(String.format(RM_COMMAND, path));
 						} else {
@@ -163,8 +136,18 @@ public class Main {
 					}
 				} catch (Exception e) {
 					System.out.println("An error occurred while compressing image: " + file.getPath());
+					hasError = true;
 				} finally {
-					totalOriginalSize += file.length();
+					// Attempt to clean up the temporary PNG images.
+					try {
+						runtime.exec(String.format(RM_COMMAND, webPPath + PNG));
+						if (needsConvertToPng) {
+							runtime.exec(String.format(RM_COMMAND, basePath + PNG));
+						}
+						if (hasError) {
+							runtime.exec(String.format(RM_COMMAND, webPPath));
+						}
+					} catch (Exception e) { /* Consume the exception. */ }
 				}
 			});
 		}
@@ -177,26 +160,49 @@ public class Main {
 		}
 	}
 
-	private static double getImageDissimilarity(final Runtime runtime, final long quality, final String path,
+	private static boolean searchForOptimalQuality(final File file, final String path, final String basePath,
+			final String webPPath, final File webPFile, final boolean needsConvertToPng) throws IOException, InterruptedException {
+		// Perform binary search to find the best WebP compression value.
+		long min = 0, max = 100;
+		while (min != max) {
+			// Generate the temporary PNG images.
+			long quality = Math.round((min + max) / 2.0);
+			if (getImageDissimilarity(quality, path, basePath, webPPath, needsConvertToPng, false) <
+					qualityThreshold && isSmallerFile(webPFile, file)) {
+				// The image can be compressed more.
+				if (max == quality) {
+					break; // Prevent infinite loops.
+				} else {
+					max = quality;
+				}
+			} else {
+				// Compression is now visible to the human eye (or the file got larger).
+				// Scale the compression back a bit (if we can).
+				if (min == quality) {
+					break; // Prevent infinite loops.
+				} else {
+					min = quality;
+				}
+			}
+		}
+		// Return true if the binary search failed to find a valid compression value.
+		return min == 100;
+	}
+
+	private static double getImageDissimilarity(final long quality, final String path,
 			final String basePath, final String webPPath, final boolean needsConvertToPng, final boolean isLossless)
-			throws InterruptedException, IOException {
-		runtime.exec(String.format(isLossless ? CWEBP_LOSSLESS_COMMAND : CWEBP_COMMAND, quality, path, webPPath)).waitFor();
-		runtime.exec(String.format(DWEBP_COMMAND, webPPath, webPPath + PNG)).waitFor();
+			throws IOException, InterruptedException {
+		Main.runtime.exec(String.format(isLossless ? CWEBP_LOSSLESS_COMMAND : CWEBP_COMMAND, quality, path, webPPath)).waitFor();
+		Main.runtime.exec(String.format(DWEBP_COMMAND, webPPath, webPPath + PNG)).waitFor();
 		if (needsConvertToPng) {
-			runtime.exec(String.format(SIPS_COMMAND, path, basePath + PNG)).waitFor();
+			Main.runtime.exec(String.format(SIPS_COMMAND, path, basePath + PNG)).waitFor();
 		}
 
 		// Read in the dissimilarity result from Butteraugli.
-		BufferedReader input = new BufferedReader(new InputStreamReader(runtime.exec(
-				String.format(COMPARE_PNGS_COMMAND, webPPath + PNG, basePath + PNG)).getInputStream()));
+		BufferedReader input = new BufferedReader(new InputStreamReader(
+				Main.runtime.exec(String.format(COMPARE_PNGS_COMMAND, webPPath + PNG, basePath + PNG)).getInputStream()));
 		double dissimilarity = Double.valueOf(input.readLine());
 		input.close();
-
-		// Clean up the temporary PNG images.
-		runtime.exec(String.format(RM_COMMAND, webPPath + PNG));
-		if (needsConvertToPng) {
-			runtime.exec(String.format(RM_COMMAND, basePath + PNG));
-		}
 		return dissimilarity;
 	}
 
