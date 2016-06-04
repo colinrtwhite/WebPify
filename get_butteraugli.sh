57306da7d58188f1b61761a8a819c5dd7a65e3b0
Colin White <colin@colinwhite.me>
2016-06-03 18:56:43 -0700

    Path directories shouldn't end with a slash.

--- get_butteraugli.sh
+++ get_butteraugli.sh
@@ -5,5 +5,5 @@ cd butteraugli/src/
 make
 cd ~
 touch .bash_profile
-echo 'export PATH=$PATH:~/butteraugli/src/' >> .bash_profile
+echo 'export PATH=$PATH:~/butteraugli/src' >> .bash_profile
 echo 'Reboot your PC to finish adding Butteraugli to your path.'
